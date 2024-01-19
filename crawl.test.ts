5e3627f3ed9bf4ad5806929d4ea524f4bfa0e875
Matheus Silva <matheus.silva@in8.com.br>
2024-01-19 18:00:51 -0300

    feat: add getUrlsFromHtml + tests

--- crawl.test.ts
+++ crawl.test.ts
@@ -1,16 +1,45 @@
-import { normalizeURL } from "./crawl";
+import { getURLsFromHTML, normalizeURL } from "./crawl";
 import { test, expect } from "@jest/globals";
 
-test("normalizeURL trailing slash", () => {
-  const input = "https://blog.boot.dev/path/";
+test("normalizeURLs", () => {
+  const input = "https://Blog.boot.dev/path/";
   const output = normalizeURL(input);
   const expected = "blog.boot.dev/path";
   expect(output).toEqual(expected);
 });
 
-test("normalizeURL capitals", () => {
-  const input = "https://Blog.boot.dev/path";
-  const output = normalizeURL(input);
-  const expected = "blog.boot.dev/path";
+test("getURLsFromHTML absolute", () => {
+  const inputHTMLBody = `<html><body><a href='https://youtube.com/'>Link</a></body></html>`;
+  const inputBaseURL = "https://youtube.com";
+  const output = getURLsFromHTML(inputHTMLBody, inputBaseURL);
+  const expected = ["https://youtube.com/"];
   expect(output).toEqual(expected);
+});
+
+test("getURLsFromHTML relative", () => {
+  const inputHTMLBody = `<html><body><a href='/video/'>Link</a></body></html>`;
+  const inputBaseURL = "https://youtube.com";
+  const output = getURLsFromHTML(inputHTMLBody, inputBaseURL);
+  const expected = ["https://youtube.com/video/"];
+  expect(output).toEqual(expected);
+});
+
+test("getURLsFromHTML both", () => {
+  const inputHTMLBody = `<html><body>
+    <a href='/video/'>Link</a>
+    <a href='https://youtube.com/video2/'>Link2</a>
+  </body></html>`;
+  const inputBaseURL = "https://youtube.com";
+  const output = getURLsFromHTML(inputHTMLBody, inputBaseURL);
+  const expected = ["https://youtube.com/video/", "https://youtube.com/video2/"];
+  expect(output).toEqual(expected);
+});
+
+test("getURLsFromHTML valid", () => {
+  const inputHTMLBody = `<html><body>
+    <a href='123invalid'>Link</a>
+  </body></html>`;
+  const inputBaseURL = "https://youtube.com";
+  const output = getURLsFromHTML(inputHTMLBody, inputBaseURL);
+  expect(output).toEqual([]);
 });
--- crawl.ts
+++ crawl.ts
@@ -1,3 +1,5 @@
+import { JSDOM } from "jsdom";
+
 export function normalizeURL(url: string): string {
   const urlObj = new URL(url);
   const hostPath = `${urlObj.hostname}${urlObj.pathname}`;
@@ -7,4 +9,19 @@ export function normalizeURL(url: string): string {
   }
 
   return hostPath;
+}
+
+export function getURLsFromHTML(htmlBody: string, baseURL: string) {
+  const dom = new JSDOM(htmlBody);
+  const anchors = dom.window.document.querySelectorAll("a");
+  const hrefs = Array.from(anchors).map((a: HTMLAnchorElement) => {
+    try {
+      if (a.href.charAt(0) === "/") {
+        return new URL(`${baseURL}${a.href}`).href;
+      }
+      return new URL(a.href).href;
+    } catch (err) {}
+  });
+
+  return hrefs;
 }
